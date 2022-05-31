1a9df2f5aa08414f84d73fbfbec92df23bf89075
rayyanaamir22 <92763211+rayyanaamir22@users.noreply.github.com>
2022-05-31 09:01:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 '''
 Name: Rayyan A
-Date: May 14, 2022
+Date: May 31, 2022
 Program: Linear Regression Calculator
 '''
 
 # Modules
 import os
-import matplotlib.pyplot as plt
 
 # Other Files
 import functions as f
@@ -43,7 +42,24 @@ def main():
     res.correlationResults()
 
     # Create a graph using matplotlib.pyplot
-    f.scatterPlot() 
+    try:
+      f.scatterPlot() 
+    except:
+      print('\nGraph is meaningless due to correlation coefficient of 0.')
+
+    # Option to use model for a prediction
+    while True:
+      print('\nDo you want to use the regression to predict a value?')
+      pre = input()
+      if pre.lower().startswith('y'):
+        os.system('clear')
+        f.predict()
+        break
+      elif pre.lower().startswith('n'):
+        os.system('clear')
+        break
+      else:
+        print('(Yes or no)')
   
     # Reuse program or exit
     if res.reuse():
@@ -57,5 +73,15 @@ def main():
 # PROGRAM START
 if __name__ == '__main__':
   main()
+
+  '''
+  Description:
+  Linear regression is a mathematical technique to yield the most accurate line of best fit regarding ALL data points entered (including outliers). This program automates the entire calculation, and it only requires the user to enter the data points they want to find the linear regression of.
+
+  How to use:
+  When run, the program will ask the user to enter each data point manually as an ordered pair. First, the user must enter the X coordinate, followed by the corresponding Y coordinate. The program assumes that each pair entered in 1 round is an ordered pair. Also, linear regression is not possible with a single data point, so the program will not begin calculations until there is more than 1 (one) data point.
+
+  Once finished, state that you are finished entering data points and the program will immediately calculate and return the linear regression, as well as several meaningful statistics constants like standard deviation and covariance.
+  '''
   
 # PROGRAM END
