d145e4b0e5b879b342c756bfad2fdc79f67dd5bf
rayyanaamir22 <92763211+rayyanaamir22@users.noreply.github.com>
2022-05-31 09:03:09 -0400

    Update results.py

--- results.py
+++ results.py
@@ -41,16 +41,16 @@ def correlationResults(): # Return the equations' results
       slope = '-x'
       roundedSlope = slope
     else:
-      slope = '{0}x' .format(e.a)
-      roundedSlope = '{0}x' .format(round(e.a, 3))
+      slope = f'{e.a}x'
+      roundedSlope = f'{round(e.a, 3)}x'
 
     # Create intercept component
     if e.b > 0: # If y-int is positive
-      intercept = ' + ' + str(e.b)
-      roundedIntercept = ' + ' +str(round(e.b, 3))
+      intercept = f' + {e.b}'
+      roundedIntercept = f' + {round(e.b, 3)}'
     elif e.b < 0: # If y-int is negative
-      intercept = ' - ' + str(abs(e.b))
-      roundedIntercept = ' - ' +str(round(abs(e.b), 3))
+      intercept = f' - {abs(e.b)}'
+      roundedIntercept = f' - {round(abs(e.b), 3)}'
     elif e.b == 0: # If y-int is zero
       intercept = ''
       roundedIntercept = intercept
@@ -58,25 +58,17 @@ def correlationResults(): # Return the equations' results
     # Concatenate equation
     equation = str(slope + intercept) # On console
     roundedEquation = str(roundedSlope + roundedIntercept) # On graph
-    print('\nLinear Regression (Line of best fit equation):\ny = ' + equation)
+    print(f'\nLinear Regression (Line of best fit equation):\ny = {equation}')
 
-  elif e.verticalLine == True: # THIS DOES NOT WORK IDKY
-    print('\nLinear Regression (Line of best fit equation): x =', f.tdataY[0])
+  else: # THIS DOES NOT WORK IDKY
+    print('Data is undefined')
 
   # Correlation coefficient
-  print('\nPearson Correlation Coefficient (r): ', e.r)
-  print('The data has {0} correlation of {1} proportionality.' .format(e.rStrength, e.rProportionality))
+  print(f'\nPearson Correlation Coefficient (r): {e.r}')
+  print(f'The data has {e.rStrength} correlation of {e.rProportionality} proportionality.')
   if e.ar == 1.0:
     print('Since |r| = 1, all data points exist on the linear function above.')
   
   # Covariance and Standard deviation
-  ''' # Not sure how to display covariance
-  print('Covariance: ', f.covariance, f.yAxis, '/', f.xAxis) # The unit of covariance is the quotient of the compared units
-  '''
-  
-  print('\nStandard Deviation (X): ', f.stdX) # Unitless
-  print('Standard Deviation (Y): ', f.stdY) # Unitless
-  
-  
-  # Algorithm result
-  # Neural network?
+  print(f'\nStandard Deviation (X): {f.stdX}') 
+  print(f'Standard Deviation (Y): {f.stdY}')
